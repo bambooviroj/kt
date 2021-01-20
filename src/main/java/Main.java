904c591eba0f1086ebbc6b48474ff0795dccd684
bamboovir <bamboo1886@gmail.com>
2021-01-20 02:44:24 -0600

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static boolean id(boolean v) {
+        return v;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(id(true));
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun id(v: Boolean): Boolean {
+    return v
+}
+
+
+fun main() {
+    println(id(true))
+}
